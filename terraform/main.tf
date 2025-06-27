51082ee5f92402a65d3c31870204e05b17225b3f
Vitor Ouabdelkader <vitorouab@gmail.com>
2025-06-27 16:08:12 -0300

    Fix ports

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,16 +29,9 @@ resource "aws_security_group" "web" {
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    from_port   = 80
+    from_port   = 5000
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
