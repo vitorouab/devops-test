d3c55692d8fac0b1fe5985031f65d6d3c4f7f16e
Vitor Ouabdelkader <vitorouab@gmail.com>
2025-06-27 16:10:02 -0300

    Fix ports for flask app

--- app/app.py
+++ app/app.py
@@ -12,5 +12,5 @@ def health():
     return "OK", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 80))
     app.run(host="0.0.0.0", port=port)
--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "web" {
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    from_port   = 5000
+    from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
